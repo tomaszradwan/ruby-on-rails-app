56523bdf4a1cefed07a0624100ab756296f76fa8
tomaszradwan <radwan.tomasz@gmail.com>
2018-01-26 21:51:11 +0100

    category | model, db

--- courses/db/schema.rb
+++ courses/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116062516) do
+ActiveRecord::Schema.define(version: 20180126203252) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.boolean "visibility", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 20
