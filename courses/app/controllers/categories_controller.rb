771c9954a9f33edd9bff5f3141f11d9fed3fff29
tomaszradwan <radwan.tomasz@gmail.com>
2018-01-29 21:55:51 +0100

    category | form-create

--- courses/app/controllers/categories_controller.rb
+++ courses/app/controllers/categories_controller.rb
@@ -1,16 +1,35 @@
 class CategoriesController < ApplicationController
+  layout false
+
   def index
+    @category = Category.sort
   end
 
   def show
   end
 
   def new
+    @category_form = Category.new({:name => 'Put category name'})
+    @counter = Category.count + 1
   end
 
   def edit
   end
 
   def delete
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to(:action => 'index')
+    else
+      @counter = Category.count + 1
+      render('new')
+    end
+  end
+
+  def category_params
+    params.require(:category_form).permit(:name, :position, :visibility)
   end
 end
--- courses/config/routes.rb
+++ courses/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "categories/index"
+  get "categories/show"
+  get "categories/new"
+  get "categories/delete"
+  get "categories/edit"
+  get "categories/create"
 end
