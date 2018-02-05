c2e5a4be69f8b022824960a1cfc282d00809f50a
tomaszradwan <radwan.tomasz@gmail.com>
2018-02-05 22:53:45 +0100

    articles |  controller, views - new, create | _form.html.erb

--- courses/app/controllers/articles_controller.rb
+++ courses/app/controllers/articles_controller.rb
@@ -6,6 +6,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @articles = Article.new({:name => 'TEST'})
+    @page = Page.order('position ASC')
+    @counter = Article.count + 1
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article '#{@article.name}' created."
+      redirect_to(:action => 'index')
+    else
+      @counter = Article.count + 1
+      @category = Article.order('position ASC')
+      render('new')
+    end
   end
 
   def edit
@@ -15,5 +30,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
+  end
+
+  def article_params
+    params.require(:article).permit(:page_id, :name, :position, :visibility,:photos)
   end
 end
--- courses/config/routes.rb
+++ courses/config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   get 'articles/index'
   get 'articles/new'
+  get 'articles/create'
   get 'articles/edit'
   get 'articles/delete'
   get 'articles/show'
